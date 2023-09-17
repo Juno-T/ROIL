018c835381f3ed3d302351be6d19a36ef206f77f
juno-t <yossathorn.tian@gmail.com>
2023-09-17 23:47:33 +0900

    prototype transition, add test, init logger

--- auto_policy_programming/policy/transition.py
+++ auto_policy_programming/policy/transition.py
@@ -1,5 +1,62 @@
+import traceback
+import types
+import logging
+
+logger = logging.getLogger(__name__)
+
 class Transition:
     # state_from
     # transition_function
     # score etc.
-    pass
+    def __init__(self, state_name_from, transition_function_code):
+        logger.debug(f"\nCreating transition from {state_name_from}\n{transition_function_code}\n"+"-"*80+"\n")
+        self.state_name_from = state_name_from
+        self.transition_function_code = transition_function_code.strip()
+        self.transition_function = None
+        self.score = None
+        self._compile_transition_function()
+    
+    def _compile_transition_function(self):
+        try:
+            self.compiled_functions = [f for f in extract_functions(compile(self.transition_function_code, "<string>", "exec"))]
+            if len(self.compiled_functions) != 1:
+                raise Exception(f"Expected 1 function, found {len(self.compiled_functions)}")
+            # self.compiled_functions = [(fname, code_obj), ...]
+            self.transition_function = types.FunctionType(self.compiled_functions[0][1], globals())
+        except Exception as e:
+            logger.exception(e)
+            raise Exception(f"Error compiling transition function:\n {e}")
+
+
+    def __call__(self, state):
+        if self.transition_function is None:
+            raise Exception("Transition function not compiled")
+        # TODO: validate & format state
+        return self.transition_function(state)
+
+
+import dis
+from itertools import islice
+
+# old itertools example to create a sliding window over a generator
+def window(seq, n=2):
+    """Returns a sliding window (of width n) over data from the iterable
+       s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...
+    """
+    it = iter(seq)
+    result = tuple(islice(it, n))
+    if len(result) == n:
+        yield result    
+    for elem in it:
+        result = result[1:] + (elem,)
+        yield result
+
+def extract_functions(codeobject):
+    codetype = type(codeobject)
+    signature = ('LOAD_CONST', 'LOAD_CONST', 'MAKE_FUNCTION', 'STORE_NAME')
+    for op1, op2, op3, op4 in window(dis.get_instructions(codeobject), 4):
+        if (op1.opname, op2.opname, op3.opname, op4.opname) == signature:
+            # Function loaded
+            fname = op2.argval
+            assert isinstance(op1.argval, codetype)
+            yield fname, op1.argval
---
+++ test/test_policy.py
@@ -0,0 +1,28 @@
+import sys
+import pytest
+from auto_policy_programming.policy.transition import Transition
+import logging
+
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+def test_transition_compile():
+    transition = Transition("state1", """
+def transition(state: str) -> str:
+    return "state2"
+""")
+    assert transition.transition_function is not None
+    assert transition("state1") == "state2"
+
+
+def test_transition_compile_error():
+    with pytest.raises(Exception):
+        transition = Transition("state1", """
+def whatever(state):
+return "state2"
+""")
+
+
+def test_transition_compile_error2():
+    with pytest.raises(Exception):
+        transition = Transition("state1", """x = 1""")
