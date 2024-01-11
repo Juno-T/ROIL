2ab6cca9e3657f7173af4efeb62c2019e433779f
juno-t <yossathorn.tian@gmail.com>
2024-01-11 18:21:53 +0900

    add webshop env

---
+++ webshop/setup.py
@@ -0,0 +1,13 @@
+# setup with setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name='webshop',
+    version='0.0.1',
+    description='Webshop environment for OpenAI Gym',
+    url="",
+    author="",
+
+    packages=find_packages(),
+)
